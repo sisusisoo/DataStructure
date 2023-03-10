8fc5b113840c26825e14f521a0d13876a178cdad
sisusisoo <khc97622@naver.com>
2023-03-10 13:19:24 +0900

    project-1

--- DataStructure/main.c
+++ DataStructure/main.c
@@ -6,9 +6,42 @@
 //
 
 #include <stdio.h>
+int prime(int maxNumber){
+    int primeNumber=0;
+    int count=0;
+    int sum=0;
+    
+    for(int i=2;i<maxNumber;i++)
+    {
+        count=0;
+        for(int j=2;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        
+        if(count==1)
+        {
+            primeNumber=i;
+            sum+=primeNumber;
+            
+            //printf("%d, ",primeNumber);
+        }
+        
+        
+  
+        
+     
+    }
+   
+    return sum;
+    
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    int i=prime(100);
+    printf("%d",i);
     return 0;
 }
