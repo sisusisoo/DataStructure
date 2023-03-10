d028ef567dc6869f2c0b3d27cc2114aa07df440a
sisusisoo <khc97622@naver.com>
2023-03-10 12:31:10 +0900

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 강현철 on 2023/03/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
